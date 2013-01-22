be9f8a8b85fc1224457bd248f5dd1abc89fc9e3a
Morten Nielsen <mn@iter.dk>
2013-01-21 16:55:49 -0800

    Update src/SharpGIS.GZipWebClient/Properties/AssemblyInfo.cs
    Updated version number to match nuget package

--- src/SharpGIS.GZipWebClient/Properties/AssemblyInfo.cs
+++ src/SharpGIS.GZipWebClient/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SharpGIS.GZipWebClient")]
-[assembly: AssemblyDescription("WebClient for Windows Phone 7 that adds support for GZIP compressed web requests.")]
+[assembly: AssemblyDescription("WebClient for Windows Phone 7/8 that adds support for GZIP compressed web requests.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Morten Nielsen")]
 [assembly: AssemblyProduct("SharpGIS.GZipWebClient")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
